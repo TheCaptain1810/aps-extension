67e2894fcbb41a137ca9938c1038ed19bf6ac62c
Pranav Pise <itspranavpise@gmail.com>
2025-07-16 15:11:21 +0530

    summary panel

--- wwwroot/extensions/SummaryExtension.js
+++ wwwroot/extensions/SummaryExtension.js
@@ -1,9 +1,13 @@
 import { BaseExtension } from "./BaseExtension.js";
+import { SummaryPanel } from "./SummaryPanel.js";
+
+const SUMMARY_PROPS = ["Length", "Area", "Volume", "Density", "Mass", "Price"];
 
 class SummaryExtension extends BaseExtension {
   constructor(viewer, options) {
     super(viewer, options);
     this._button = null;
+    this._panel = null;
   }
 
   load() {
@@ -18,18 +22,36 @@ class SummaryExtension extends BaseExtension {
       this.removeToolbarButton(this._button);
       this._button = null;
     }
+    if (this._panel) {
+      this._panel.setVisible(false);
+      this._panel.uninitialize();
+      this._panel = null;
+    }
     console.log("SummaryExtension unloaded");
     return true;
   }
 
   onToolbarCreated() {
+    this._panel = new SummaryPanel(
+      this,
+      "model-summary-panel",
+      "Model Summary"
+    );
     this._button = this.createToolbarButton(
       "summary-button",
       "https://img.icons8.com/small/32/brief.png",
       "Show Summary"
     );
     this._button.onClick = () => {
-      //TODO
+      this._panel.setVisible(!this._panel.isVisible());
+      this._button.setState(
+        this._panel.isVisible()
+          ? Autodesk.Viewing.UI.Button.State.ACTIVE
+          : Autodesk.Viewing.UI.Button.State.INACTIVE
+      );
+      if (this._panel.isVisible()) {
+        this.update();
+      }
     };
   }
 
@@ -49,7 +71,21 @@ class SummaryExtension extends BaseExtension {
   }
 
   async update() {
-    //TODO
+    if (this._panel) {
+      const selectedIds = this.viewer.getSelection();
+      const isolatedIds = this.viewer.getIsolatedNodes();
+      if (selectedIds.length > 0) {
+        // If any nodes are selected, compute the aggregates for them
+        this._panel.update(this.viewer.model, selectedIds, SUMMARY_PROPS);
+      } else if (isolatedIds.length > 0) {
+        // Or, if any nodes are isolated, compute the aggregates for those
+        this._panel.update(this.viewer.model, isolatedIds, SUMMARY_PROPS);
+      } else {
+        // Otherwise compute the aggregates for all nodes
+        const dbids = await this.findLeafNodes(this.viewer.model);
+        this._panel.update(this.viewer.model, dbids, SUMMARY_PROPS);
+      }
+    }
   }
 }
 
